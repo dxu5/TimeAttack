aa4d7e0c049eff04e129c2bd09c69e35b2adb191
dxu5 <derek_xu@brown.edu>
2020-11-01 15:49:53 -0800

    Started skeleton and basic canvas setup

---
+++ public/javascripts/main.js
@@ -0,0 +1,28 @@
+import SpriteSheet from "./sprite_sheet.js";
+import { loadImage, loadLevel } from "./image_loader.js";
+
+const drawBackground = (background, context, sprites) => {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let i = x1; i < x2; i++) {
+      for (let j = y1; j < y2; j++) {
+        sprites.drawTile(background.tile, context, i, j);
+      }
+    }
+  });
+};
+
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+
+loadImage("/image/tiles.png").then((image) => {
+  const sprites = new SpriteSheet(image, 15, 15);
+  sprites.define("ground1", 3, 215);
+  sprites.define("ground2", 3, 198);
+  sprites.define("sky", 105, 15);
+
+  loadLevel("1-1").then((level) => {
+    level.backgrounds.forEach((background) =>
+      drawBackground(background, context, sprites)
+    );
+  });
+});
